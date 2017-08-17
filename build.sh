b0ab47b3e08968151dfaa6e5c2901552f463e44a
coolshou <coolshou2060@gmail.com>
2017-08-18 02:39:24 +0800

    add linux build.sh

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+ARCH=`uname -m`
+
+echo "Build $ARCH"
+if [ -e build ]; then
+    rm -rf build
+fi
+if [ -e dist/qperf_$ARCH ];then
+    rm dist/qperf_$ARCH
+fi
+python3 -m PyInstaller qperf.spec
+mv dist/qperf dist/qperf_$ARCH
+
+#echo "Build x86"
+#rm -rf build
+#rm dist/qperf
+#python3 -m PyInstaller qperf.spec
